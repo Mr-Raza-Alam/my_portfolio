bd91f000e3d77b8e0424cecfcff34f0a852d8318
Raza_Web Development <razabtech27@gmail.com>
2026-07-11 13:32:22 +0530

    get some changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => (
       <div className="nav-links">
         <a href="#about">About</a>
         <a href="#experience">Experience</a>
+        <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
         <a href="#education">Education</a>
         <a href="#certifications">Certifications</a>
